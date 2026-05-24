58d8916f662502e344820b27411e24fe09f2c267
nndy21 <andinurcahyana21@gmail.com>
2026-05-24 20:48:01 +0700

    tugas 2.py

---
+++ # Program Operasi Perhitungan.py
@@ -0,0 +1,31 @@
+# Program Operasi Perhitungan
+
+# Angka yang digunakan
+angka1 = 10
+angka2 = 16 
+
+# Penjumlahan
+hasil_tambah = angka1 + angka2
+print("Hasil penjumlahan =", hasil_tambah)
+
+# Pengurangan
+hasil_kurang = angka1 - angka2
+print("Hasil pengurangan =", hasil_kurang)
+
+# Perkalian
+hasil_kali = angka1 * angka2
+print("Hasil perkalian =", hasil_kali)
+
+# Pembagian
+hasil_bagi = angka1 / angka2
+print("Hasil pembagian =", hasil_bagi)
+
+# Pangkat
+hasil_pangkat = angka1 ** angka2
+print("Hasil pangkat =", hasil_pangkat)
+
+# Sisa bagi
+hasil_sisa = angka1 % angka2
+print("Hasil sisa bagi =", hasil_sisa)
+
+print("\nProgram selesai dijalankan")
